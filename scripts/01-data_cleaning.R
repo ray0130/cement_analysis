d5f307cc75f309dd1d2539ad74a3df1ac65fd072
Ray <yenray.wen@gmail.com>
2022-02-27 16:35:30 -0500

    final edit

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -22,19 +22,11 @@ library(tidyverse)
 library(dplyr)
 library(kableExtra)
 library(readstata13)
-library(labelled)
 library(haven)
 
 # Load data from dta file
 
 cement <- read.dta13("inputs/cement_update2020.dta")
-
-# Below is not necessary
-# cement_update2020 <- read_dta("inputs/cement_update2020.dta")
-# 
-# cement_names <- tibble(variables = names(cement),
-#                  full_names = var_label(cement_update2020))
-# cement_names <- data.frame(lapply(cement_names, as.character), stringsAsFactors=FALSE)
 
 
          
